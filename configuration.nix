c6cb4c4cca129ea16f9e1a20b27ad8cb1db9c7fa
carnotweat <jasitis@gmail.com>
2022-10-29 16:39:19 +0530

    minor

--- configuration.nix
+++ configuration.nix
@@ -8,12 +8,42 @@ let
 in
 {
   environment.systemPackages = with pkgs; [
-    #nginx
+    unstable.emacs
+    nginx
+    tetex
+    bibtex2html
+    rubber
+    bibtex2html
+    gnupg
+    hci
+    libtool
+    nix-prefetch-docker
+    sqlite
+    tinc_pre
+    zlib
+    poppler
+    gcc
+    automake
+    cmake
+    gnumake
+    autoconf
+    hy
+    nox
+    stack
+    cabal-install
+#    android-tools
     unstable.certbot
     unstable.nixops
-    unstable.emacs
     firefox
+    pmbootstrap
     git
+    gnumake
+    killall
+    streamlink
+    home-manager
+    #osquery
+    python27Full
+    python39Full
     
   ];
 
@@ -23,6 +53,12 @@ in
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+
+  # Nix_Path
+
+  nix.nixPath = [
+  "nixpkgs=${toString pkgs.path}"
+];
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -46,10 +82,9 @@ in
   i18n.supportedLocales = [ "all" ];
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  #emacs
-  #services.emacs.package = pkgs.emacsUnstable;
-  #nixpkgs.overlays = [ (import self.inputs.emacs-overlay) ];
-
+  services.autorandr.enable = true;
+  services.acpid.enable = true;
+  services.acpid.lidEventCommands = "/run/current-system/sw/bin/autorandr --batch --change";
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
@@ -94,13 +129,10 @@ in
     isNormalUser = true;
     description = "x";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      nixopsUnstable
-      git
-      emacs
-    #  thunderbird
-    ];
+    # packages = with pkgs; [
+      
+    #   nixopsUnstable
+    # ];
   };
 
   # Enable automatic login for the user.
@@ -108,8 +140,12 @@ in
   services.xserver.displayManager.autoLogin.user = "x";
   #virtualbox
   virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "x" ];
+  users.extraGroups.vboxusers.members = [ "x" "root" ];
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "python2.7-pyjwt-1.7.1"
+  ];
+
   #virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;  
   virtualisation.virtualbox.guest.enable = true;
@@ -142,12 +178,26 @@ in
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
+  programs.gnupg.agent.pinentryFlavor = "emacs";
+  programs.gnupg.agent.enable = true;
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
+    # grafana configuration
+  services.grafana = {
+    enable = true;
+    domain = "grafana.pele";
+    port = 2342;
+    addr = "127.0.0.1";
+  };
+    # nginx reverse proxy
+  services.nginx.virtualHosts.${config.services.grafana.domain} = {
+    locations."/" = {
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyWebsockets = true;
+    };
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
